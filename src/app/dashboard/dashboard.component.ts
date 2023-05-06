2659d521fe5ae4c53fcfa5197443a8584075735a
sinanmv <sinanmv234@gmail.com>
2023-05-06 13:18:14 +0530

    tailwindproject

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,16 +8,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'student', collage: 'collage', mark: 800},
-  {position: 2, name: 'student', collage: 'collage', mark: 800},
-  {position: 3, name: 'student', collage: 'collage', mark: 800},
-  {position: 4, name: 'student', collage: 'collage', mark: 800},
-  {position: 5, name: 'student', collage: 'collage', mark: 800},
-  {position: 6, name: 'student', collage: 'collage', mark: 800},
-  {position: 7, name: 'student', collage: 'collage', mark: 800},
-  {position: 8, name: 'student', collage: 'collage', mark: 800},
-  {position: 9, name: 'student', collage: 'collage', mark: 800},
-  {position: 10, name: 'student',collage: 'collage', mark: 800},
+  {position: 1, name: 'Rahul', collage: 'MS College', mark: 794},
+  {position: 2, name: 'Raja', collage: 'VK College', mark: 791},
+  {position: 3, name: 'Aswin', collage: 'VK College', mark: 790},
+  {position: 4, name: 'Raju', collage: 'COC College', mark: 788},
+  {position: 5, name: 'Mohan', collage: 'MS College', mark: 778},
+  {position: 6, name: 'Rani', collage: 'MS College', mark: 771},
+  {position: 7, name: 'Das', collage: 'VK COllege', mark: 770},
+  {position: 8, name: 'Unni', collage: 'COC College', mark: 761},
+  {position: 9, name: 'Akhil', collage: 'COC College', mark: 756},
+  {position: 10, name: 'Mithun',collage: 'MS College', mark: 754},
 ];
 @Component({
   selector: 'app-dashboard',
